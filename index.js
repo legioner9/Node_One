7e9d3d1b333cae775e28685684767ed3a789fcb7
STM <legioner9>
2019-09-23 22:44:00 +0700

    4 start listen after success connect db

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = require('_/app');
 const conf = require('_/config');
 const port = +conf.PORT;
 connect_mdb.promise_mdb()
-    .then(() => {
-        console.log('Yess');
+    .then((info) => {
+        let base_i = {base_i_name_: info.name, base_i_host_: info.host, base_i_port_: info.port};
+        console.log('info_int :: ', base_i);
         app.listen(port, () => console.log(`Listening on ${port} port!)`));
     })
-    .catch(e =>
-        console.log(e));
+    .catch(e => {
+        console.log(e);
+        process.exit(1);
+    });
 
 
