32b74364ebde3d29280ff481f6f6625275e585ea
STM <legioner9>
2019-09-23 12:36:46 +0700

    4 start listen after success connect db

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-let test;
-test = 45;
-console.log(test);
-let data = 'Hello Ej))';
-const arr_init = ['go', 'and', 'see'];
-
 const connect_mdb = require('_/db');
-console.log('connect_mdb :: ', connect_mdb);
+console.log('connect_mdb new :: ', connect_mdb);
 
+const app = require('_/app');
 const conf = require('_/config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
 const port = +conf.PORT;
+connect_mdb.promise_mdb()
+    .then(() => {
+        console.log('Yess');
+        app.listen(port, () => console.log(`Listening on ${port} port!)`));
+    })
+    .catch(e =>
+        console.log(e));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('index', {data: data, arr_init: arr_init})
-});
-
-app.get('/create', function (req, res) {
-    res.render('create');
-});
-
-app.post('/create', function (req, res) { //eslint-disable-line prettier/prettier
-    console.log('Request::', req);
-    let owe_data = req.body.text;
-    res.render('owe', {owe_data: owe_data});
-    arr_init.push(owe_data);
-});
-app.listen(port, () => console.log(`Listening on ${port} port!)`));
